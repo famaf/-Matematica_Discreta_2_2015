46aec969e3c326da7561cb96b38ffb4b0a4d3eb2
Mario E. Ferreyra <marioezequiel.ferreyra@yahoo.com.ar>
2015-05-12 00:35:15 -0300

    Esqueleto MUY basico del la estructura que carga el grafo

--- api.c
+++ api.c
@@ -10,3 +10,22 @@ struct GrafoPlus
     u32 lados;
     
 };
+
+
+/*
+struct ESTRUCTURA
+{
+    int identificador;
+    unsigned  int *vecinos;
+    unsigned int orden_ingresado[vertices_count];
+    unsigned int grado_x_vertice[vertices_count]
+};
+
+typedef struct Grafoplus
+{
+    unsigned int vertices_count;
+    unsigned int edges_count;
+    ESTRUCTURA lista_vertices[vertices_count];
+} GrafoSt;
+
+*/
